724d110f53fe6ec35361dfccdb36be3070c110f4
scarescrow <sagnik.sinharoy@gmail.com>
2017-07-22 04:46:20 +0530

    Add task and details endpoints

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
 ## Introduction
----
 
 This app is designed to keep track of items to do weekly. User can enter tasks by categories that are to be done every week, along with number of hours to put in for every week. The goal is for the app to design a daily timetable that can be followed.
 
 **Disclaimer:** This app has been designed to get hands on experience with React and Firebase.
 
 ## Technologies
----
 
 * Front End: ReactJS
 * Back End: NodeJS
 * Database: Firebase
 
 ## Steps to Build
----
 
-1. Clone the source ````$ git clone https://github.com/scarescrow/grand-frere.git````
+1. Clone the source ````$ git clone https://github.com/scarescrow/Donna.git````
 2. Install all required packages by running command ````$ npm install```` or ````$ yarn add````
 3. Create a new app in Firebase which will host the site and hold the data.
 4. Rename ````src/firebase-config-example.js```` to ````src/firebase-config.js```` and change all config values to the values provided by the new firebase app.
@@ -25,6 +22,5 @@ This app is designed to keep track of items to do weekly. User can enter tasks b
 8. Run ````$ firebase deploy```` to upload app to the web. Navigate to the app public url to find the running version of the app.
 
 ## License
----
 
 Licensed under [MIT License](LICENSE)
--- src/index.js
+++ src/index.js
@@ -8,32 +8,52 @@ import './index.css';
 firebase.initializeApp(firebase_config);
 const database = firebase.database();
 
+function getValues(snap) {
+	const values = snap.val();
+	var fbValues = []
+	if(values !== null && values !== undefined) {
+		var sortable = [];
+		for(var v in values) {
+			sortable.push([v, values[v]])
+		}
+		sortable.sort(function(a, b) {
+			return b[1] - a[1];
+		})
+		for (var i = 0; i < sortable.length; i ++)
+			fbValues.push(sortable[i][0]);
+	}
+	return fbValues;
+		
+}
+
 function getCategories(callback) {
 	database.ref('/categories').once('value', snap => {
-		const categories = snap.val();
-		var fbCategories = []
-		if(categories !== null && categories !== undefined) {
-			var sortable = [];
-			for(var c in categories) {
-				sortable.push([c, categories[c]])
-			}
-			sortable.sort(function(a, b) {
-				return b[1] - a[1];
-			})
-			for (var i = 0; i < sortable.length; i ++)
-				fbCategories.push(sortable[i][0]);
-		}
+		var fbCategories = getValues(snap);
 		callback(fbCategories);
 	});
 }
 
-function CategoryItem(props) {
+function getTasks(filter, callback) {
+	var dbRef = database.ref('/tasks');
+	if(filter !== "all") {
+		dbRef = dbRef.orderByChild("category").equalTo(filter);
+	}
+	dbRef.once('value', snap => {
+		var fbTasks = getValues(snap);
+		callback(fbTasks);
+	});
+}
+
+function ListItem(props) {
 	var removeStyle = {
 		paddingLeft: '80px',
 		color:'red'
 	}
 	return (
-		<li>{props.category}<span className="remove" style={removeStyle} onClick={() => props.remove(props.category)}>X</span></li>
+		<li>
+			<Link to={props.url + props.value}>{props.value}</Link>
+			<span className="remove" style={removeStyle} onClick={() => props.remove(props.value)}>X</span>
+		</li>
 	)
 }
 
@@ -96,7 +116,7 @@ class Categories extends React.Component {
 	render() {
 		var categoryTags = [];
 		this.state.categories.forEach((item, i) => {
-			categoryTags.push(<CategoryItem key={i} category={item} remove={this.removeCategory}></CategoryItem>)
+			categoryTags.push(<ListItem key={i} value={item} url="/tasks/" remove={this.removeCategory}></ListItem>)
 		});
 		return (
 			<div className="categoryList">
@@ -107,6 +127,138 @@ class Categories extends React.Component {
 				<input type="text" id="addCategoryInput" placeholder="Add Category" />
 				<input type="text" id="addPriorityInput" placeholder="Priority" onKeyPress={(e) => this._handleKeyPress(e)}/>
 				<button type="button" onClick={() => this.addCategory()}>Add</button>
+			</div>
+		);
+	}
+}
+
+class AddTask extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			categories: []
+		}
+	}
+
+	fillCategories() {
+		getCategories((fbCategories) => {
+			this.setState({categories: fbCategories});
+		});
+	}
+
+	watchCategory() {
+		database.ref('/categories').on('child_removed', (snap) => {
+			this.fillCategories();
+		});
+		database.ref('/categories').on('child_added', (snap) => {
+			this.fillCategories();
+		})
+	}
+
+	componentDidMount() {
+		this.fillCategories();
+		this.watchCategory();
+	}
+
+	addTask() {
+		var task = document.getElementById('addTaskInput').value;
+		var category = document.getElementById('categoryDropdown').value;
+		var desc = document.getElementById('taskDescription').value.split(';');
+		if(task === "" || category === "")
+			return;
+		var task_item = {}
+		task_item[task] = {
+			"category": category,
+			"description": desc
+		}
+		database.ref('/tasks').update(task_item).then(() => {
+			document.getElementById('addTaskInput').value = "";
+			document.getElementById('taskDescription').value = "";
+			document.getElementById('addTaskInput').focus();
+		}).catch((error) => {
+			console.log("There was an error while updating tasks in Firebase: " + error);
+		});
+	}
+
+	_handleKeyPress(e) {
+		if(e.key === 'Enter') {
+			this.addTask();
+		}
+	}
+
+	render() {
+		var categoryTags = [];
+		this.state.categories.forEach((item, i) => {
+			categoryTags.push(<option key={i} value={item}>{item}</option>)
+		});
+		return (
+			<div className="categorySelect">
+				<div>
+					<input type="text" id="addTaskInput" placeholder="Add Task" />
+				</div>
+				<div>
+					<select id="categoryDropdown">
+						{categoryTags}
+					</select>
+				</div>
+				<div>
+					<textarea id="taskDescription" onKeyPress={(e) => this._handleKeyPress(e)}></textarea>
+				</div>
+				<div>
+					<button type="button" onClick={() => this.addTask()}>Add</button>
+				</div>
+			</div>
+		)
+	}
+
+}
+
+class Tasks extends React.Component {
+	constructor(props) {
+		super();
+		this.state = {
+			tasks: [],
+			filter: props.filter
+		}
+	}
+
+	fillTasks() {
+		getTasks(this.state.filter, (fbTasks) => {
+			this.setState({tasks: fbTasks});
+		});
+	}
+
+	watchTasks() {
+		database.ref('/tasks').on('child_removed', (snap) => {
+			this.fillTasks();
+		});
+		database.ref('/tasks').on('child_added', (snap) => {
+			this.fillTasks();
+		})
+	}
+
+	componentDidMount() {
+		this.fillTasks();
+		this.watchTasks();
+	}
+
+	removeTask(task) {
+		database.ref('/tasks/' + task).remove().catch((error) => {
+			console.log("There was an error while deleting task '" + task + "': " + error);
+		});
+	}
+
+	render() {
+		var taskTags = [];
+		this.state.tasks.forEach((item, i) => {
+			taskTags.push(<ListItem key={i} value={item} url="/taskDetail/" remove={this.removeTask}></ListItem>)
+		});
+		return (
+			<div className="categoryList">
+				<h3>Current Tasks</h3>
+				<ul>
+					{taskTags}
+				</ul>
 			</div>
 		);
 	}
@@ -126,7 +278,7 @@ function Header(props) {
 			<nav>
 				<ul>
 					<li>
-						<Link to="/tasks">Tasks</Link>
+						<Link to="/tasks/all">Tasks</Link>
 					</li>
 				</ul>
 			</nav>
@@ -137,7 +289,7 @@ function Header(props) {
 function Home(props) {
 	return (
 		<div>
-			<h1>Welcome to Grand Frere</h1>
+			<h1>Welcome to Donna</h1>
 			<Header></Header>
 			<Categories></Categories>
 		</div>
@@ -146,8 +298,48 @@ function Home(props) {
 
 function TaskHandler(props) {
 	return (
-		<div>You reached tasks</div>
+		<div>
+			<Tasks filter={props.match.params.filter}></Tasks>
+			<AddTask></AddTask>
+		</div>
 	)
+}
+
+class TaskDetailHandler extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			task: props.match.params.task,
+			category: "",
+			desc: []
+		}
+	}
+
+	componentDidMount() {
+		database.ref('tasks/' + this.state.task + '/').once('value', snap => {
+			var values = snap.val();
+			this.setState({
+				category: values['category'],
+				desc: values['description']
+			});
+		});
+	}
+
+	render() {
+		var descTags = [];
+		this.state.desc.forEach((item, i) => {
+			descTags.push(<div key={i}>{item}</div>);
+		});
+		return (
+			<div>
+				<h1>{this.state.task}</h1>
+				<div>Category: {this.state.category}</div>
+				<h4>Details:</h4>
+				<div>{descTags}</div>
+			</div>
+		);
+	}
 }
 
 function MainRoutes(props) {
@@ -155,7 +347,8 @@ function MainRoutes(props) {
 		<main>
 			<Switch>
 				<Route exact path="/" component={Home}/>
-				<Route path="/tasks" component={TaskHandler}/>
+				<Route path="/tasks/:filter" component={TaskHandler}/>
+				<Route path="/taskDetail/:task" component={TaskDetailHandler}/>
 			</Switch>
 		</main>
 	)
